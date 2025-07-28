556c6208ad30dadd402b8fe2b01328cb0fd285b4
Milena Kachko <kachkomilena@gmail.com>
2025-07-28 16:24:03 +0200

    add hw

---
+++ src/components/App/App.tsx
@@ -0,0 +1,57 @@
+import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "../VoteStats/VoteStats";
+import Notification from "../Notification/Notification";
+import CafeInfo from "../test/CafeInfo";
+import type { Votes, VoteType } from "../../types/votes";
+import "./App.module.css";
+import { useState } from "react";
+
+function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleVote = (type: VoteType) => {
+    setVotes((prevVotes) => ({
+      ...prevVotes,
+      [type]: prevVotes[type] + 1,
+    }));
+  };
+
+  const resetVote = () => {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+  const positiveRate =
+    totalVotes > 0 ? Math.round((votes.good / totalVotes) * 100) : 0;
+
+  return (
+    <>
+      <CafeInfo />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVote}
+        canReset={totalVotes > 0}
+      />
+
+      {totalVotes > 0 ? (
+        <VoteStats
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={positiveRate}
+        />
+      ) : (
+        <Notification />
+      )}
+    </>
+  );
+}
+
+export default App;
